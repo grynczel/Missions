d963a97238449ee7c12ff4fa51c22617c195559f
Baptiste Degryse <baptiste.degryse@hotmail.com>
2014-10-14 20:22:04 +0200

    calculator 1 essai

--- Mission 2/Mission2_new/src/Calculator.java
+++ Mission 2/Mission2_new/src/Calculator.java
@@ -2,5 +2,47 @@
 
 public class Calculator {
 
-	public static 
+	public Calculator() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static LinkedBinaryTree<String> derive(LinkedBinaryTree<String> t){
+		LinkedBinaryTree<String> t2=null;
+		switch(t.element()){
+		case "+": t2= new LinkedBinaryTree<String>("+",derive(t.leftTree()),derive(t.rightTree()));
+			break;
+		case "-": t2= new LinkedBinaryTree<String>("-",derive(t.leftTree()),derive(t.rightTree()));
+			break;
+		case "*": t2= new LinkedBinaryTree<String>("+",
+				new LinkedBinaryTree<String>("*",derive(t.leftTree()),t.rightTree()),
+				new LinkedBinaryTree<String>("*",t.leftTree(),derive(t.rightTree())));
+			break;
+		case "/": t2= new LinkedBinaryTree<String>("/",
+				new LinkedBinaryTree<String>("-",
+					new LinkedBinaryTree<String>("*",derive(t.leftTree()),t.rightTree()),
+					new LinkedBinaryTree<String>("*",t.leftTree(),derive(t.rightTree()))),
+				new LinkedBinaryTree<String>("*",t.rightTree(),t.rightTree()));
+			break;
+		case "^": t2= new LinkedBinaryTree<String>("*",
+					new LinkedBinaryTree<String>("^",
+						t.leftTree(),
+						new LinkedBinaryTree<String>("-",t.rightTree(),
+													new LinkedBinaryTree<String>("1",null,null))),
+					t.rightTree());
+			break;
+		case "sin": t2= new LinkedBinaryTree<String>("*",
+				new LinkedBinaryTree<String>("cos",t.leftTree(),null),
+				derive(t.leftTree()));
+			break;
+		case "cos": t2= new LinkedBinaryTree<String>("*",
+			new LinkedBinaryTree<String>("-1",null,null),
+			new LinkedBinaryTree<String>("*",
+				new LinkedBinaryTree<String>("sin",t.leftTree(),null),
+				derive(t.leftTree())));
+			break;
+		}
+		return t2;
+		
+	}
 }
