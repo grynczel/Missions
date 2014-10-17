f73a88c83b7667fcfe5dcc06299050b3dc4b8a96
Baptiste Degryse <baptiste.degryse@hotmail.com>
2014-10-17 15:49:29 +0200

    derivee partielles en plus

--- Mission 2/Mission2_new/src/Calculator.java
+++ Mission 2/Mission2_new/src/Calculator.java
@@ -4,7 +4,14 @@ public class Calculator {
 	/**
 	 * @author Baptiste Degryse
 	 */
-	
+	public static String var="x";//pour deriver par rapport a 
+					//d'autres variables si on le souhaite
+	/**
+	 * calcule la derivee
+	 * @param t : l'arbre representant une expression analytique a deriver
+	 * @return un arbre representant une expression analytique contenant 
+	 * la derivee du l'arbre fourni en argument
+	 */
 	public static LinkedBinaryTree<String> derive(LinkedBinaryTree<String> t){
 		LinkedBinaryTree<String> t2=null;
 		
@@ -47,15 +54,14 @@ public class Calculator {
 				new LinkedBinaryTree<String>("sin",t.leftTree(),null),
 				derive(t.leftTree())));
 		
-		else if(t.element().equals("x"))
+		else if(t.element().equals(var))
 			t2= new LinkedBinaryTree<String>("1",null,null);
 		
 		else if(t.element().matches("-?\\d+"))//nombre
 			t2= new LinkedBinaryTree<String>("0",null,null);
 		
 		else
-			System.out.println("element inconnu : "+ t.element());
-		
+			t2= new LinkedBinaryTree<String>("0",null,null);		
 		
 		return t2;
 		
