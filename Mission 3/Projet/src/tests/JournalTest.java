c5b551c30c617a21d8164167428f6276eeecc982
Baptiste Degryse <baptiste.degryse@hotmail.com>
2014-10-29 20:37:16 +0100

    commentaires en plus, retrait de journal.getMap() (inutile)

--- Mission 3/Projet/src/tests/JournalTest.java
+++ Mission 3/Projet/src/tests/JournalTest.java
@@ -11,7 +11,7 @@ public class JournalTest {
 	@Test
 	public void test() {
 		Journal journal=new Journal(1,"Journals.csv", ",");
-		Entree e = journal.getMap().get("Journal of Global Business Management");
+		Entree e = journal.get("Journal of Global Business Management");
 		assertEquals("C", e.get("Rank"));
 		
 	}
