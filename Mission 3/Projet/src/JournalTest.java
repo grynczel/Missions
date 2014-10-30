b29d3c20c5e864cd1d614bcfe03b6a7eaa17edf9
Jérôme Bertaux <bertauxjeje@hotmail.com>
2014-10-30 11:36:31 +0100

    Ajout test unitaire
    Ajout d’un test pour les virgules dans le titre et d’un autre pour les
    virgules dans les FoR names

--- Mission 3/Projet/src/JournalTest.java
+++ Mission 3/Projet/src/JournalTest.java
@@ -16,5 +16,21 @@ public class JournalTest {
 		assertEquals("C", e.get("Rank"));
 		
 	}
+	
+	@Test
+	public void testVirguleTitre(){
+		Journal journal=new Journal(1,"Journals.csv", ",");
+		Entree e = journal.get("Genes, Brain and Behavior");
+		assertEquals("B", e.get("Rank"));
+		
+	}
+	
+	@Test
+	public void testVirguleFoR(){
+		Journal journal=new Journal(1,"Journals.csv", ",");
+		Entree e = journal.get("The Review of Financial Studies");
+		assertEquals("Banking, Finance and Investment", e.get("FoR1 Name"));
+		
+	}
 
 }
