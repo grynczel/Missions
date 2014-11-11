c3880e237b19bdad2999f6762be7d57ba55ce4ce
grynczel <grynczel.wojciech@gmail.com>
2014-11-11 14:11:39 +0100

    Revert projet mission 3

--- Mission 3/Projet/src/Journal.java
+++ Mission 3/Projet/src/Journal.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,77 +11,66 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+
 public class Journal {
 	/**
 	 * @author Baptiste Degryse, Wojciech Grynczel, Jerome Bertaux
 	 */
-	private Map<String, Entree> map = new TreeMap<String, Entree>();
+	private Map<String,Entree> map = new TreeMap<String, Entree>();
 
 	/**
-	 * cree un journal en lisant un fichier. La premiere ligne represente les
-	 * titres des champs
-	 * 
-	 * @param indexCle
-	 *            : commence a zero, index de la cle primaire
-	 * @param filename
-	 *            : nom du fichier a lire (ou filepath)
-	 * @param delimiter
-	 *            : delimiter entre chaque valeurs
+	 * cree un journal en lisant un fichier.
+	 * La premiere ligne represente les titres des champs
+	 * @param indexCle : commence a zero, index de la cle primaire
+	 * @param filename : nom du fichier a lire (ou filepath)
+	 * @param delimiter : delimiter entre chaque valeurs
 	 */
-	public Journal(int indexCle, String filename, String delimiter) {
+	public Journal(int indexCle, String filename, String delimiter){
 		BufferedReader fileReader = null;
-		char escapeChar = '"';
+		char escapeChar='"';
 		String[] tokens;
-		int index = 1;
+		int index=1;
 		try {
 			String line = "";
 			// Create the file reader
 			fileReader = new BufferedReader(new FileReader(filename));
 			line = fileReader.readLine();
-			if (line == null)
+			if(line==null)
 				return;
 			String[] cle = line.split(delimiter);
-			int size = cle.length;
+			int size=cle.length;
 
 			// Read the file line by line
 			while ((line = fileReader.readLine()) != null) {
 				index++;
 				// Get all tokens available in line
 
-				if (!line.equals("")) {
+				if(!line.equals("")){
 					tokens = line.split(delimiter);
-					try {
-						String[] to = new String[size];
-						int decalage = 0;
-						for (int i = 0; i < cle.length
-								&& i + decalage < tokens.length; i++) {
-							to[i] = tokens[i + decalage];
-							if (tokens[i + decalage].charAt(0) == escapeChar
-									&& tokens[i + decalage].charAt(tokens[i
-											+ decalage].length() - 1) == escapeChar) {
-							}// guillemets mais pas de virgule dedans
-							else if (tokens[i + decalage].charAt(0) == escapeChar) {
-								int j = 1;
-								to[i] = tokens[i + decalage].substring(1);
-								while (tokens[i + decalage + j].charAt(tokens[i
-										+ decalage + j].length() - 1) != escapeChar) {
-									to[i] = to[i] + ","
-											+ tokens[i + decalage + j];
+					try{
+						String []to=new String[size];
+						int decalage=0;
+						for(int i=0;i<cle.length && i+decalage+1<tokens.length;i++){
+							to[i]=tokens[i+decalage];
+							if(tokens[i+decalage].charAt(0)==escapeChar && 
+									tokens[i+decalage].charAt(tokens[i+decalage].length()-1)==escapeChar){}//guillemets mais pas de virgule dedans
+							else if(tokens[i+decalage].charAt(0)==escapeChar){
+								int j=1;
+								to[i]=tokens[i+decalage].substring(1);
+								while(tokens[i+decalage+j].charAt(tokens[i+decalage+j].length()-1)!=escapeChar){
+									to[i]=to[i]+","+tokens[i+decalage+j];
 									j++;
 								}
-								to[i] = to[i]
-										+ ","
-										+ tokens[i + decalage + j].substring(0,
-												tokens[i + decalage + j]
-														.length() - 1);
-								decalage += j;
+								to[i]=to[i]+","+tokens[i+decalage+j].substring(0, tokens[i+decalage+j].length()-1);
+								decalage+=j;
 							}
 						}
-						tokens = to;
+						tokens=to;
 
-						map.put(tokens[indexCle], new Entree(cle, tokens));
-					} catch (Exception e) {
-						System.out.println("Erreur : " + index + " " + line);
+						map.put(tokens[indexCle], new Entree(cle,tokens));
+					}
+					catch(Exception e){
+						System.out.println("Erreur : "+index+" "+line);
 						e.printStackTrace();
 					}
 				}
@@ -94,54 +84,19 @@ public class Journal {
 				e.printStackTrace();
 			}
 		}
-		// Object[] a = map.values().toArray();
+		//Object[] a = map.values().toArray();
 	}
-
 	/**
 	 * retourne l'entree ayant comme cle cle
-	 * 
-	 * @param cle
-	 *            : cle de l'entree
+	 * @param cle : cle de l'entree
 	 * @return l'entree de cle cle
 	 */
-	public Entree get(String cle) {
+	public Entree get(String cle){
 		return map.get(cle);
 	}
 
 	public Map<String, Entree> getMap() {
 		return map;
 	}
-
-	public List<Entree> getSortedList(String orderBy) {
-		List<Entree> list = new ArrayList<Entree>(map.values());
-		return getSortedList(list, orderBy);
-	}
-	
-	private List<Entree> getSortedList(List<Entree> list, String orderBy) {
-		Collections.sort(list, new EntreeComparable(orderBy));
-		return list;
-	}
-	
-	public List<Entree> getSortedList(String orderBy, String field, String fieldValue) {
-		List<Entree> filteredList = new ArrayList<Entree>();
-		for (Entree entree : new ArrayList<Entree>(map.values())) {
-			if (entree.get(field).compareTo(fieldValue) == 0) {
-				filteredList.add(entree);
-			}
-		}
-		return getSortedList(filteredList, orderBy);
-	}
-
-	public class EntreeComparable implements Comparator<Entree> {
-		private String orderBy;
-
-		public EntreeComparable(String orderBy) {
-			this.orderBy = orderBy;
-		}
-
-		@Override
-		public int compare(Entree arg0, Entree arg1) {
-			return arg0.get(orderBy).compareTo(arg1.get(orderBy));
-		}
-	}
 }
+
--- Mission 3/Projet/src/Main.java
+++ Mission 3/Projet/src/Main.java
@@ -21,29 +21,20 @@ public class Main {
 		jT.testVirguleFoR();
 		jT.testVirguleTitre();
 
-		//System.out.println(journal.getMap());
-		endTime = System.nanoTime();
-		long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
-		System.out.println("Init time : " + duration/1000000 +"ms" );
-		
-		List<Entree> list = journal.getSortedList("Title", "Rank", "A*");
-		System.out.println(list);
-		
-//		//System.out.println(journal.getMap().get("Journal of Global Business Management"));
-//		do{	
-//			System.out.print("Entrez le nom d'un journal : ");
-//			titre = scan.nextLine();
-//			
-//			Entree entree = journal.get(titre);
-//			if(entree == null){
-//				System.out.println("Not found!");
-//			}else{
-//				System.out.println(journal.get(titre));
-//			}
-//			System.out.print("Continuer (y/n) ? ");
-//			q = scan.nextLine().charAt(0);
-//		}while(q == 'y' || q == 'Y');
-//		scan.close();
+		do{	
+			System.out.print("Entrez le nom d'un journal : ");
+			titre = scan.nextLine();
+			
+			Entree entree = journal.get(titre);
+			if(entree == null){
+				System.out.println("Not found!");
+			}else{
+				System.out.println(journal.get(titre));
+			}
+			System.out.print("Continuer (y/n) ? ");
+			q = scan.nextLine().charAt(0);
+		}while(q == 'y' || q == 'Y');
+		scan.close();
 
 	}
 }
