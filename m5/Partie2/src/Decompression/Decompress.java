7a44d93cfbf1382534c945f320b8c416ee0eb978
Jérôme Bertaux <bertauxjeje@hotmail.com>
2014-11-25 11:29:40 +0100

    Debut decompress

---
+++ m5/Partie2/src/Decompression/Decompress.java
@@ -0,0 +1,16 @@
+
+public class Decompress {
+
+	public static void main(String[] args) {
+		if(args.length == 2){
+			
+			String fileCompress = args[0];
+			String outputFile = args[1];
+			
+		}else{
+			System.out.println("Usage: java Decompress <Compress file> <Output file>");
+		}
+
+	}
+
+}
