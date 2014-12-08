dce078ac4873666ef709f347d2a6fc94c9953d76
Baptiste Degryse <baptiste.degryse@hotmail.com>
2014-12-08 18:05:43 +0100

    squelette projet eclipse2

--- m6/Partie2/src/GraphD.java
+++ m6/Partie2/src/GraphD.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 
 public class GraphD {
 
+	public GraphD(ArrayList<String> ar){
+		
+	}
 }
--- m6/Partie2/src/Main.java
+++ m6/Partie2/src/Main.java
@@ -1,7 +1,36 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+
 
 
 public class Main {
+	/**
+	 * @author Baptiste Degryse
+	 * @param args
+	 */
+	private static String filename;
 	public static void main(String args[]){
+
+		String dir="";
+		JFileChooser fc = new JFileChooser();
+		// Demonstrate "Open" dialog:
+		fc.setCurrentDirectory(new File("."));
+		int rVal = fc.showOpenDialog(null);
+		if (rVal == JFileChooser.APPROVE_OPTION) {
+			dir=fc.getCurrentDirectory().toString()+"/"+fc.getSelectedFile().getName();
+		}
+		if (rVal == JFileChooser.CANCEL_OPTION) {
+			dir="";
+		}
+
+		if(!dir.equals(""))
+			filename=dir;
+		else
+			System.exit(1);
 		
+		ArrayList<String> ar=ReadWrite.mRead(filename);
+		GraphD graph=new GraphD(ar);
 	}
 }
